4fb55e88d004175eaecf23d535c52cf517e9b26c
Bohdan Khodakivskyi <bohdan.kh@gmail.com>
2017-03-31 15:15:25 +0400

    flow add, restructure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './component/App/index';
 import './index.css';
 
 ReactDOM.render(
